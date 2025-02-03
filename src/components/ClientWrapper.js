b956f9105d5cc7d7d5c9a62592ac036b8dbb6dfc
amirhossein <amirhossein611@gmail.com>
2025-02-03 11:34:01 +0330

    animation complete

--- src/components/ClientWrapper.js
+++ src/components/ClientWrapper.js
@@ -1,23 +1,44 @@
 /* eslint-disable react/prop-types */
 'use client';
-import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import React, { Suspense } from 'react';
 
 const ClientWrapper = ({ children }) => {
-  const pathname = usePathname(); // دریافت مسیر فعلی
+  const pathname = usePathname();
+  const [isPageExiting, setIsPageExiting] = useState(false);
+
+  useEffect(() => {
+    setIsPageExiting(false); // وقتی مسیر تغییر می‌کند، حالت صفحه به حالت عادی برمی‌گردد.
+  }, [pathname]);
+
+  const handleExitComplete = () => {
+    setIsPageExiting(true);
+  };
 
   return (
-    <AnimatePresence mode='wait'>
-      <motion.div
-        key={pathname} // تغییر صفحه باعث اجرای انیمیشن می‌شود
-        initial={{ opacity: 0, x: 50 }} // ورود از راست
-        animate={{ opacity: 1, x: 0 }} // نمایش کامل
-        exit={{ opacity: 0, x: -50 }} // خروج به چپ
-        transition={{ duration: 0.5, ease: 'easeInOut' }} // تنظیم سرعت انیمیشن
-      >
-        {children}
-      </motion.div>
+    <AnimatePresence mode='wait' onExitComplete={handleExitComplete}>
+      <Suspense>
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, x: 50 }} // ورود از راست
+          animate={{ opacity: 1, x: 0 }} // انیمیشن نمایش کامل
+          exit={{ opacity: 0, x: -50 }} // انیمیشن خروج صفحه قبلی
+          transition={{
+            duration: 0.5,
+            ease: 'easeInOut',
+          }}
+          onAnimationComplete={() => {
+            if (isPageExiting) {
+              // زمانی که انیمیشن خروج تمام شد، صفحه جدید نمایش داده می‌شود.
+              setIsPageExiting(false);
+            }
+          }}
+        >
+          {children}
+        </motion.div>
+      </Suspense>
     </AnimatePresence>
   );
 };
